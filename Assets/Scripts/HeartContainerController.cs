55c2e48390680c97ed545c54d7ecd1e9866af2af
thedaruma <rgiorda1@gmail.com>
2018-04-01 14:36:15 -0400

    added itween, hearts, coin rush container

---
+++ Assets/Scripts/HeartContainerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartContainerController : MonoBehaviour {
+	public GameObject Heart;
+	public List<GameObject> Hearts;
+	public int CurrentHealth;
+	public int MaxHealth;
+	// Use this for initialization
+	void Start () {
+		CurrentHealth = MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
