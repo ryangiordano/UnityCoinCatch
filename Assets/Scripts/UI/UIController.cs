d76c15beb908e0a3ba3d2b6b0755fd181548f7e4
thedaruma <rgiorda1@gmail.com>
2018-04-04 21:34:45 -0400

    added game loop.  Fixed bug causing lighting to malfunction on subsequent playthroughs

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+	public Button backButton;
+	// Use this for initialization
+	void Start () {
+		var btn = backButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void TaskOnClick() {
+        SceneManager.LoadScene("Main");
+
+	} 
+}
