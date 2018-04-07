ab58512341a9938403e01e97a52b255854b357ca
thedaruma <rgiorda1@gmail.com>
2018-04-06 22:35:28 -0400

    add cloud stuff

---
+++ Assets/Scripts/Background/Cloud.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x+(.3f*Time.deltaTime),transform.position.y,transform.position.z);
+	}
+}
